843047b6664574851a8373c5c9910df39438f750
Jim Toy <jim.toy@gmail.com>
2024-11-28 18:37:45 -0500

    Day 1 Part 2 solution

--- src/day1/Main.java
+++ src/day1/Main.java
@@ -1,12 +1,30 @@
 package day1;
 
+import javax.sound.midi.Soundbank;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
+
+    static Map<String, String> lookup = new HashMap<>();
+
+
     public static void main(String[] args) throws IOException {
+
+        lookup.put("eight", "ei8ht");
+        lookup.put("two", "t2o");
+        lookup.put("one", "o1e");
+        lookup.put("three", "th3ee");
+        lookup.put("four", "f4ur");
+        lookup.put("five", "f5ve");
+        lookup.put("six", "s6x");
+        lookup.put("seven", "se7en");
+        lookup.put("nine", "n9ne");
+
         int total = 0;
         List<String> lines = Files.readAllLines(Paths.get("src/day1/input"));
         for (String line : lines) {
@@ -16,7 +34,29 @@ public class Main {
     }
 
     public static int getValue(String input) {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
+        String orginal = input;
+
+        while (true) {
+
+            String first = "";
+            int firstIndex = 1000;
+            for (Map.Entry<String, String> key : lookup.entrySet()) {
+                int position = input.indexOf(key.getKey());
+                if (position > -1 && position < firstIndex) {
+                    first = key.getKey();
+                    firstIndex = position;
+                }
+            }
+
+            if (firstIndex == 1000) {
+                break;
+            } else {
+                input = input.replaceAll(first, lookup.get(first));
+            }
+
+        }
+
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             try {
@@ -29,8 +69,8 @@ public class Main {
         String min = sb.substring(0, 1);
         String max = sb.substring(sb.length() - 1, sb.length());
 
-        int value = Integer.parseInt(min + max);
-        return value;
+        System.out.println("Input: " + orginal + " Value:" + min + max);
+        return Integer.parseInt(min + max);
 
     }
 
---
+++ src/day1/README.md
@@ -0,0 +1,5 @@
+I saw a lot of comments about this challenge.  
+A few videos I watched that got the answer correct but I could not get a correct answer even following their code.
+I admit I used a technique I found on another solution that, at first, seems a bit hacky but it works. I guess it is not 
+too terrible to leverage assumed knowledge to achieve an outcome.
+
